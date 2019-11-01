5f2fd787efcdb3754c012e6dd83e403b4f29c83d
gord0213b <gord0213@algonquinlive.com>
2019-11-01 14:05:06 -0400

    dsa

--- src/main/java/ca/areswg/shadowdestiny/blocks/AdamasBlock.java
+++ src/main/java/ca/areswg/shadowdestiny/blocks/AdamasBlock.java
@@ -7,7 +7,6 @@ import net.minecraft.block.material.Material;
 public class AdamasBlock extends Block {
     public AdamasBlock() {
         super(Properties.create(Material.IRON)
-                .sound(SoundType.METAL)
                 .hardnessAndResistance(2.0f)
                 .lightValue(14));
         setRegistryName("adamasblock");
